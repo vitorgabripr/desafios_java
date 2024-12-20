e6ad5d85100096671de5a4f12625837479ce2585
Vitor <vitor.gabrielll0245@gmail.com>
2024-12-19 21:50:27 -0300

    Comecando desafio 2

---
+++ OneDrive/Área de Trabalho/desafios_java/src/app/desafio2.java
@@ -0,0 +1,8 @@
+package src.app;
+
+public class desafio2 {
+    public static void main(String[] args) {
+        System.out.println("MENU:\n1. Gerenciar Voos\n2. Gerenciar passageiros" +
+                "\n3. Reservas\n4. Relatórios\n5. Configurações\n6. Sair");
+    }
+}
